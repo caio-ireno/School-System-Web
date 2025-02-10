9749bf39fa5f58a24fdae085d6612a0818ab4c7f
caioireno <caio_ireno@hotmail.com>
2025-02-10 15:15:47 -0300

    first commit

---
+++ src/shared/environment/index.ts
@@ -0,0 +1,3 @@
+export const Environment = {
+  URL_BASE: 'http://127.0.0.1:8000/api/',
+}
---
+++ src/shared/services/axios/index.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+import { Environment } from '../../environment'
+
+const Api = axios.create({
+  baseURL: Environment.URL_BASE,
+})
+
+export { Api }
